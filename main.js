66bc85e169a1dac906578dd6cefcdefaaa901594
QuanDoan <quandoan@QuanDoans-MacBook-Air.local>
2023-12-21 22:51:50 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const data = [
+    42,
+    'Twenty20',
+    '123',
+    -68,
+    true,
+    False,
+    'true',
+    0]
+    
+    for(i = 0; i < data.length; i++) {
+        console.log("Type of " + data[i] + " is " + typeof(data[i]))
+    }
